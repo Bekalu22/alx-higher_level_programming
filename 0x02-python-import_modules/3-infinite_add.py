3fc50d96b4cc3ce05774e6f772cd37704df3231a
Bekalu22 <97958977+Bekalu22@users.noreply.github.com>
2022-12-01 20:22:54 +0300

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-import sys
-
-
-def main(*argv):
-    l = len(sys.argv)
-    sum = 0
-    if l > 1:
-        for args in sys.argv:
-            if args != sys.argv[0]:
-                sum = sum + int(args)
-    print(sum)
-
 if __name__ == "__main__":
-    main()
+    import sys
+    argv = sys.argv[1:]
+    argv_count = len(argv)
+    index = 1
+    res = 0
+    while index <= argv_count:
+        res += int(sys.argv[index])
+        index += 1
+    print("{:d}".format(res))
