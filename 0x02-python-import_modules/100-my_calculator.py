6ed60fa17fc53f5d7f5f4bbec5efcfc972884aa7
bekalu <bekisisay2020@gmail.com>
2022-12-03 08:26:55 -0800

    add calc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,7 +2,7 @@
 from sys import argv
 from calculator_1 import add, sub, mul, div
 if __name__ != "__main__":
-        exit()
+    exit()
 argc = len(argv) - 1
 if argc != 3:
     print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
