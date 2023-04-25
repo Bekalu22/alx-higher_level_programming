eb3081eff93ed0ca05904e679e3035336cf50a6a
Bekalu22 <97958977+Bekalu22@users.noreply.github.com>
2023-04-25 22:38:57 +0300

    Delete 0x14-javascript-web_scraping directory

--- 0x14-javascript-web_scraping/0x12-javascript-warm_up/7-multi_c.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const MyVar = process.argv[2];
-if (isNaN(MyVar) === true) {
-  console.log('Missing number of occurrences');
-} else {
-  for (let i = 0; i < MyVar; i++) {
-    console.log('C is fun');
-  }
-}
--- 0x14-javascript-web_scraping/0x12-javascript-warm_up/README.md
+++
@@ -1,1 +0,0 @@
-# Java-Script Warm Up
